99dbf6680629649b459fa630de20808d881e3f35
Vlad <78107312+YaremaV@users.noreply.github.com>
2021-08-07 22:40:03 +0300

    Исправил работу Промиса, удалив команду catch()

--- JS/exercise-1.js
+++ JS/exercise-1.js
@@ -5,8 +5,9 @@ const delay = ms => {
   })
 };
 
-const logger = time => console.log(`Resolved after ${time}ms`).catch((err) => console.log(err));
+const logger = time => console.log(`Resolved after ${time}ms`);
 
-delay(2000).then(logger); // Resolved after 2000ms
-delay(1000).then(logger); // Resolved after 1000ms
-delay(1500).then(logger); // Resolved after 1500ms
+delay(2000).then(logger) // Resolved after 2000ms
+delay(1000).then(logger) // Resolved after 1000ms
+delay(1500).then(logger) // Resolved after 1500ms
+
